cf45231efe3362c12d5dd6d2de1d39fb92b81221
Tom Gobich <tom@tomgobich.com>
2022-05-27 17:09:15 -0400

    updating affiliates

--- app/Services/AffiliateService.ts
+++ app/Services/AffiliateService.ts
@@ -1,17 +1,7 @@
 export default class AffiliateService {
-  public static affiliates = [{
-    href: 'https://200995oeov2n3kc3tzw9lbnyey.hop.clickbank.net',
-    leaderboard: 'affiliates/diy-graphic-assets/diy-graphic-assets-728x90.png',
-    square: 'affiliates/diy-graphic-assets/diy-graphic-assets-336x280.png',
-    tall: 'affiliates/diy-graphic-assets/diy-graphic-assets-160x600.png',
-    altText: 'DIY Graphic Design Assets promo image'
-  }, {
-    href: 'https://cb4201hgi9yq4sa70a5228px2i.hop.clickbank.net',
-    leaderboard: 'affiliates/keyword-researcher/keyword-researcher-leader.jpg',
-    square: 'affiliates/keyword-researcher/keyword-researcher-square.jpg',
-    // tall: 'affiliates/keyword-researcher/',
-    altText: 'Long tail keyword researcher promo image'
-  }]
+  public static affiliates = [
+    Affiliate('interserver')
+  ]
 
   public static getRandom() {
     const index = this.getRandomInt()
@@ -21,4 +11,8 @@ export default class AffiliateService {
   private static getRandomInt(max: number = this.affiliates.length) {
     return Math.floor(Math.random() * max)
   }
+}
+
+function Affiliate(name, ads: { [x: string]: string } = { tall: 'tall', square: 'square', leaderboard: 'leaderboard' }) {
+  return { name, ...ads }
 }
