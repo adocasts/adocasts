1d978e5396568b39d41956df19fbe5578015e64a
Tom Gobich <tom@tomgobich.com>
2022-07-05 08:12:23 -0400

    removing field names that could be pre-populated by password savers from honeypot

--- config/honeypot.ts
+++ config/honeypot.ts
@@ -30,7 +30,7 @@ export const honeypotConfig: HoneypotConfig = {
   | honeypot component.
   |
   */
-  fields: ['ohbother', 'ohhoney', 'ohpiglet', 'ohtigger', 'ohpoo', 'firstName', 'lastName'],
+  fields: ['ohbother', 'my_name'],
 
   /*
   |--------------------------------------------------------------------------
