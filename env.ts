c5d8f622e3ed2763d93c20b4abd32377e70c3819
Tom Gobich <tom@tomgobich.com>
2023-10-27 19:16:21 -0400

    adding ASSET_DOMAIN to allow prefixing a domain before all image paths. allows using direct CDN URLs instead of hitting the GCS SDK for every image on every request locally.

--- env.ts
+++ env.ts
@@ -28,6 +28,7 @@ export default Env.rules({
 	SESSION_DRIVER: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local', 'gcs'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+	ASSET_DOMAIN: Env.schema.string.optional({ format: 'url' }),
 
 	// @adonisjs/lucid
 	DB_CONNECTION: Env.schema.string(),
