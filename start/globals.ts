0443d01dce397537448a57fde75fdc002aa728da
Tom Gobich <tom@tomgobich.com>
2023-11-22 11:18:48 -0500

    closes #80 - adding footer component

--- start/globals.ts
+++ start/globals.ts
@@ -3,10 +3,14 @@ import env from '#start/env'
 import edge from "edge.js"
 import { edgeIconify, addCollection } from 'edge-iconify'
 import { icons as phicons } from '@iconify-json/ph'
+import { icons as simpleicons } from '@iconify-json/simple-icons'
+import { DateTime } from "luxon"
 
 addCollection(phicons)
+addCollection(simpleicons)
 
 edge.use(edgeIconify)
 
+edge.global('DateTime', DateTime)
 edge.global('utils', UtilityService)
 edge.global('assetDomain', env.get('ASSET_DOMAIN', ''))
