88b4438ebb2ba45f96950dc857ef11666fee91ae
Tom Gobich <tom@tomgobich.com>
2023-11-24 15:32:21 -0500

    adding animation

--- resources/js/_alpine.js
+++ resources/js/_alpine.js
@@ -2,4 +2,27 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
+Alpine.data('mouseParallax', function (refNames = [], transforms = '') {
+  return {
+    init() {
+      document.addEventListener('mousemove', this.onMove.bind(this))
+    },
+
+    destroy() {
+      document.removeEventListener('mousemove', this.onMove.bind(this))
+    },
+
+    onMove(event) {
+      refNames.map(ref => {
+        const el = this.$refs[ref]
+        const position = el.getAttribute("value") || 5
+        const x = (window.innerWidth - event.pageX * position) / 90
+        const y = (window.innerHeight - event.pageY * position) / 90
+
+        el.style.transform = `${transforms} translateX(${x}px) translateY(${y}px)`
+      })
+    }
+  }
+})
+
 Alpine.start()
