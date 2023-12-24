3d7217749f8b0fdf004aa7f94a88d44010d4df5a
Tom Gobich <tom@tomgobich.com>
2023-12-23 19:53:28 -0500

    moving starfield into traditional alpine named data. fixing topic link on series.show

--- resources/js/_stars.js
+++ resources/js/_stars.js
@@ -1,32 +1,37 @@
 import Starback from 'starback'
+import Alpine from 'alpinejs'
 
-window.starfield = (canvasLine, canvasDot) => {
-  if (canvasLine) {
-    new Starback(canvasLine, {
-      type: 'line',
-      quantity: 25,
-      speed: 5,
-      frequency: 75,
-      slope: { x: -1, y: 10 },
-      directionX: 1,
-      speed: 3,
-      spread: -3,
-      width: canvasLine.parentElement.clientWidth,
-      height: canvasLine.parentElement.clientHeight,
-      backgroundColor: 'transparent',
-    })
+Alpine.data('starfield', () => {
+  return {
+    init() {
+      if (this.$refs.canvasLine) {
+        new Starback(this.$refs.canvasLine, {
+          type: 'line',
+          quantity: 25,
+          speed: 5,
+          frequency: 75,
+          slope: { x: -1, y: 10 },
+          directionX: 1,
+          speed: 3,
+          spread: -3,
+          width: this.$refs.canvasLine.parentElement.clientWidth,
+          height: this.$refs.canvasLine.parentElement.clientHeight,
+          backgroundColor: 'transparent',
+        })
+      }
+    
+      if (this.$refs.canvasDot) {
+        new Starback(this.$refs.canvasDot, {
+          type: 'dot',
+          quantity: 100,
+          direction: 200,
+          starSize: [0,1],
+          width: this.$refs.canvasDot.parentElement.clientWidth,
+          height: this.$refs.canvasDot.parentElement.clientHeight,
+          backgroundColor: 'transparent',
+          randomOpacity: true,
+        })
+      }
+    }
   }
-
-  if (canvasDot) {
-    new Starback(canvasDot, {
-      type: 'dot',
-      quantity: 100,
-      direction: 200,
-      starSize: [0,1],
-      width: canvasDot.parentElement.clientWidth,
-      height: canvasDot.parentElement.clientHeight,
-      backgroundColor: 'transparent',
-      randomOpacity: true,
-    })
-  }
-}
+})
