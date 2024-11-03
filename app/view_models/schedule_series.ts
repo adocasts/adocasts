693c0ecae566e8f0882b082d48450a9939904e2a
Tom Gobich <tom@tomgobich.com>
2024-11-03 06:55:29 -0500

    fixed(schedule): switch percentages from getters to set values

--- app/view_models/schedule_series.ts
+++ app/view_models/schedule_series.ts
@@ -4,20 +4,12 @@ export default class ScheduleSeriesVM {
   declare difficulty: string
   declare status: string
   declare lessonsDone: number
+  declare lessonsDonePercent: number
   declare lessonsInProgress: number
+  declare lessonsInProgressPercent: number
   declare lessonsPlanned: number
   declare lessonsTotal: number
   declare modulesTotal: number
-
-  get lessonInProgressPercent() {
-    if (!this.lessonsTotal) return 0
-    return (this.lessonsInProgress / this.lessonsTotal) * 100
-  }
-
-  get lessonsDonePercent() {
-    if (!this.lessonsTotal) return 0
-    return (this.lessonsDone / this.lessonsTotal) * 100
-  }
 
   constructor(record: any) {
     const doneKeys = ['lessons_count__done']
@@ -43,9 +35,22 @@ export default class ScheduleSeriesVM {
     this.lessonsInProgress = this.#sumCounts(record, inProgressKeys)
     this.lessonsTotal = Number.parseInt(record.meta.lessons_count)
     this.modulesTotal = Number.parseInt(record.meta.modules_count)
+
+    this.lessonsDonePercent = this.#getLessonsDonePercent()
+    this.lessonsInProgressPercent = this.#getLessonsInProgressPercent()
   }
 
   #sumCounts(record: any, keys: string[]) {
     return keys.reduce((sum, key) => sum + Number.parseInt(record.meta[key]), 0)
+  }
+
+  #getLessonsDonePercent() {
+    if (!this.lessonsTotal) return 0
+    return (this.lessonsDone / this.lessonsTotal) * 100
+  }
+
+  #getLessonsInProgressPercent() {
+    if (!this.lessonsTotal) return 0
+    return (this.lessonsInProgress / this.lessonsTotal) * 100
   }
 }
