0bd7391576d3869ec299b1684b2cd962fe304b7a
Tom Gobich <tom@tomgobich.com>
2025-03-30 09:05:23 -0400

    design: stubbing series filters

--- app/controllers/series_controller.ts
+++ app/controllers/series_controller.ts
@@ -1,26 +1,20 @@
 import { seriesIndexValidator } from '#validators/series'
 import type { HttpContext } from '@adonisjs/core/http'
 import GetSeriesRecentlyUpdated from '../actions/series/get_series_recently_updated.js'
+import GetTopics from '../actions/topics/get_topics.js'
 
 export default class SeriesController {
   async index({ view, request }: HttpContext) {
-    const {
-      page = 1,
-      sort = 'latest',
-      topics = [],
-    } = await request.validateUsing(seriesIndexValidator)
+    const filters = await request.validateUsing(seriesIndexValidator)
 
-    if (page === 1) {
-      const latest = await GetSeriesRecentlyUpdated.fromCache({ limit: 5 })
-      view.share({ latest })
-    }
-
+    const latest = await GetSeriesRecentlyUpdated.fromCache({ limit: 5 })
+    const topics = await GetTopics.fromCache()
     const series = await GetSeriesRecentlyUpdated.fromCache({
       withPosts: true,
       postLimit: 5,
     })
 
-    return view.render('pages/series/index', { series })
+    return view.render('pages/series/index', { latest, series, topics })
   }
 
   async show({}: HttpContext) {}
