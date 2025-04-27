5c592eb4dc948318296aa08c77168d73b0268e4c
Tom Gobich <tom@tomgobich.com>
2025-04-27 09:17:09 -0400

    adding supplementary auth logic

--- app/auth/actions/store_session_signin.ts
+++ app/auth/actions/store_session_signin.ts
@@ -4,26 +4,78 @@ import BaseAction from '#core/actions/base_action'
 import User from '#user/models/user'
 import { HttpContext } from '@adonisjs/core/http'
 import { Infer } from '@vinejs/vine/types'
+import { errors } from '@adonisjs/auth'
+import { Session } from '@adonisjs/session'
+import stripe from '#plan/services/stripe_service'
+
+type Validator = Infer<typeof signInValidator>
 
 export default class StoreSessionSignIn extends BaseAction {
+  forwardIgnore = ['signin', 'signup', 'users/menu']
   validator = signInValidator
 
-  async asController(
-    { response, auth, session }: HttpContext,
-    { options, ...data }: Infer<typeof this.validator>
-  ) {
+  async asController({ response, auth, session }: HttpContext, { options, ...data }: Validator) {
     if (await AuthAttempt.disallows(data.uid)) {
       session.flashErrors({
         form: 'Your account has been locked due to repeated bad login attempts. Please reset your password.',
       })
       return response.redirect('/forgot-password')
     }
-
-    const user = await User.verifyCredentials(data.uid, data.password)
+    console.log('here')
+    const user = await this.#verifyCredentials(data)
 
     await auth.use('web').login(user, options.remember)
     await AuthAttempt.clear(data.uid)
 
-    return response.redirect().toRoute('home')
+    const redirect = await this.#getRedirectLocation(options, session)
+    const checkout = await this.#checkForPlan(user, options)
+
+    if (checkout.bail) {
+      session.flash(checkout.status, checkout.message)
+      return checkout.redirect ? response.redirect(checkout.redirect) : response.redirect().back()
+    }
+
+    session.flash('success', `Welcome back, ${user.handle}!`)
+
+    return response.redirect(redirect)
+  }
+
+  async #verifyCredentials({ uid, password }: Pick<Validator, 'uid' | 'password'>) {
+    try {
+      return await User.verifyCredentials(uid, password)
+    } catch (error) {
+      console.log({ error })
+      if (error instanceof errors.E_INVALID_CREDENTIALS) {
+        AuthAttempt.recordBadLogin(uid)
+      }
+      throw error
+    }
+  }
+
+  async #getRedirectLocation(options: Validator['options'], session: Session) {
+    if (this.forwardIgnore.some((path) => options.forward?.includes(path))) {
+      options.forward = '/'
+    }
+
+    switch (options.action) {
+      case 'email_verification':
+        return session.get('email_verification')
+      case 'cms':
+        return 'https://cms.adocasts.com'
+      default:
+        return options.forward ?? '/'
+    }
+  }
+
+  async #checkForPlan(user: User, options: Validator['options']) {
+    if (!options.plan) return { bail: false, status: '', message: '' }
+
+    const { status, message, checkout } = await stripe.tryCreateCheckoutSession(user, options.plan)
+
+    if (['error', 'warning'].includes(status)) {
+      return { status, message, bail: true }
+    }
+
+    return { status, message, bail: true, redirect: checkout!.url! }
   }
 }
--- app/auth/actions/store_session_signup.ts
+++ app/auth/actions/store_session_signup.ts
@@ -1,18 +1,57 @@
 import { signUpValidator } from '#auth/validators/auth'
 import BaseAction from '#core/actions/base_action'
+import stripe from '#plan/services/stripe_service'
 import User from '#user/models/user'
 import { HttpContext } from '@adonisjs/core/http'
 import { Infer } from '@vinejs/vine/types'
 
+type Validator = Infer<typeof signUpValidator>
+
 export default class StoreSessionSignUp extends BaseAction {
+  forwardIgnore = ['signin', 'signup', 'users/menu']
   validator = signUpValidator
 
-  async asController(
-    { response, auth, session }: HttpContext,
-    { options, ...data }: Infer<typeof this.validator>
-  ) {
+  async asController({ response, auth, session }: HttpContext, { options, ...data }: Validator) {
     const user = await User.create(data)
-    
+
     await user.related('profile').create({})
+    await auth.use('web').login(user, options.remember)
+
+    const redirect = await this.#getRedirectLocation(options)
+    const checkout = await this.#checkForPlan(user, options)
+
+    if (checkout.bail) {
+      session.flash(checkout.status, checkout.message)
+      return checkout.redirect ? response.redirect(checkout.redirect) : response.redirect().back()
+    }
+
+    session.flash('success', `Welcome to Adocasts, ${user.handle}!`)
+
+    return response.redirect(redirect)
+  }
+
+  async #getRedirectLocation(options: Validator['options']) {
+    if (this.forwardIgnore.some((path) => options.forward?.includes(path))) {
+      options.forward = '/'
+    }
+
+    switch (options.action) {
+      case 'cms':
+        return 'https://cms.adocasts.com'
+      default:
+        return options.forward ?? '/'
+    }
+  }
+
+  async #checkForPlan(user: User, options: Validator['options']) {
+    if (!options.plan) return { bail: false, status: '', message: '' }
+
+    const { status, message, checkout } = await stripe.tryCreateCheckoutSession(user, options.plan)
+
+    if (['error', 'warning'].includes(status)) {
+      return { status, message, bail: true }
+    }
+
+    return { status, message, bail: true, redirect: checkout!.url! }
   }
 }
