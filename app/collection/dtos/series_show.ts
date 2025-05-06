57921adbc1c7dec160b6f02e6ca2faa241b9970d
Tom Gobich <tom@tomgobich.com>
2025-05-06 07:26:42 -0400

    series.show -> next up & repository url

--- app/collection/dtos/series_show.ts
+++ app/collection/dtos/series_show.ts
@@ -1,6 +1,7 @@
 import AssetDto from '#asset/dtos/asset'
 import Collection from '#collection/models/collection'
 import ProgressableDto from '#core/dtos/progressable_dto'
+import { ProgressContext } from '#core/middleware/context/_progress'
 import SeriesLessonDto from '#post/dtos/series_lesson'
 import ProgressTypes from '#progress/enums/progress_types'
 import TopicDto from '#taxonomy/dtos/topic'
@@ -27,6 +28,18 @@ export class SeriesShowDto extends ProgressableDto {
   declare modules: ModuleDto[]
   declare meta: Record<string, any>
 
+  get postIds() {
+    if (!this.lessons && !this.modules) return []
+
+    const modulePostIds = this.modules.reduce<number[]>(
+      (acc, module) => [...acc, ...(module.lessons?.map((post) => post.id) || [])],
+      []
+    )
+    const postIds = this.lessons?.map((post) => post.id) ?? []
+
+    return [...modulePostIds, ...postIds]
+  }
+
   constructor(series?: Collection) {
     super()
 
@@ -46,5 +59,30 @@ export class SeriesShowDto extends ProgressableDto {
     this.modules = ModuleDto.fromArray(series.children)
     this.lessons = SeriesLessonDto.fromArray(series.posts)
     this.meta = series.$extras
+  }
+
+  findNextUserLesson(progress: ProgressContext) {
+    if (!this.postIds?.length) return
+
+    for (const id of this.postIds) {
+      const progression = progress.post.get(id)
+
+      // if post is completed, skip
+      if (progression?.isCompleted) continue
+
+      // find post within series
+      let lesson = this.lessons.find((post) => post.id === id)
+
+      // if post is found, return it
+      if (lesson) return lesson
+
+      // find post within modules
+      for (const module of this.modules) {
+        lesson = module.lessons.find((post) => post.id === id)
+        if (lesson) return lesson
+      }
+    }
+
+    return this.lessons?.at(0) ?? this.modules?.at(0)?.lessons?.at(0)
   }
 }
