1727393feff59587f4895ef6cf303a3af63c89af
Tom Gobich <tom@tomgobich.com>
2025-08-30 22:39:47 -0400

    unwrap og render

--- app/actions/general/render_og_image.ts
+++ app/actions/general/render_og_image.ts
@@ -7,37 +7,32 @@ import puppeteer from 'puppeteer'
 
 export default class RenderOgImage extends BaseAction {
   async asController({ response, view, params }: HttpContext) {
-    try {
-      const type = params.entity
-      const slug = params.slug
-      const entity = await this.#getEntity(type, slug)
+    const type = params.entity
+    const slug = params.slug
+    const entity = await this.#getEntity(type, slug)
 
-      // 1. Render an Edge view to HTML
-      // Create a dedicated Edge template for your OG image
-      const htmlContent = await view.render('pages/og', { entity, type })
-      // return htmlContent
-      // 2. Launch Puppeteer and generate the image
-      const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'], // Important for production environments
-      })
+    // 1. Render an Edge view to HTML
+    // Create a dedicated Edge template for your OG image
+    const htmlContent = await view.render('pages/og', { entity, type })
+    // return htmlContent
+    // 2. Launch Puppeteer and generate the image
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'], // Important for production environments
+    })
 
-      const page = await browser.newPage()
+    const page = await browser.newPage()
 
-      await page.setViewport({ width: 1200, height: 630 }) // Standard OG image size
-      await page.goto('data:text/html;charset=UTF-8,' + encodeURIComponent(htmlContent), {
-        waitUntil: 'networkidle0',
-      })
+    await page.setViewport({ width: 1200, height: 630 }) // Standard OG image size
+    await page.goto('data:text/html;charset=UTF-8,' + encodeURIComponent(htmlContent), {
+      waitUntil: 'networkidle0',
+    })
 
-      const screenshotBuffer = await page.screenshot({ type: 'png' }) // or 'jpeg'
+    const screenshotBuffer = await page.screenshot({ type: 'png' }) // or 'jpeg'
 
-      await browser.close()
+    await browser.close()
 
-      response.header('Content-Type', 'image/png')
-      response.send(screenshotBuffer)
-    } catch (error) {
-      console.error('Error generating OG image:', error)
-      response.status(500).send('Could not generate OG image.')
-    }
+    response.header('Content-Type', 'image/png')
+    response.send(screenshotBuffer)
   }
 
   async #getEntity(entity: string, slug: string) {
