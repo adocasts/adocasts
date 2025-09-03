fce9f266e9ee25ed2201d84ebe1d0bf2a4299364
Tom Gobich <tom@tomgobich.com>
2025-09-03 19:00:19 -0400

    fix: type error

--- database/migrations/1756936957625_alter_posts_add_updated_content_ats_table.ts
+++ database/migrations/1756936957625_alter_posts_add_updated_content_ats_table.ts
@@ -10,6 +10,6 @@ export default class extends BaseSchema {
   }
 
   async down() {
-    this.schema.alterTable(this.tableName, (table) => {})
+    this.schema.alterTable(this.tableName, (_table) => {})
   }
 }
