333b88e46fcb20975c8844c9fc8bda9d692a4d15
Tom Gobich <tom@tomgobich.com>
2025-09-13 14:30:15 -0400

    fix: use this.schema.raw instead of this.raw in migration; applied to prod db outside of migration so this doesn't need to rerun

--- database/migrations/1757765853677_alter_discussions_idx_created_ats_table.ts
+++ database/migrations/1757765853677_alter_discussions_idx_created_ats_table.ts
@@ -4,10 +4,10 @@ export default class extends BaseSchema {
   protected tableName = 'discussions'
 
   async up() {
-    this.raw('CREATE INDEX idx_discussions_created_at ON discussions (created_at DESC)')
+    this.schema.raw('CREATE INDEX idx_discussions_created_at ON discussions (created_at DESC)')
   }
 
   async down() {
-    this.raw('DROP INDEX idx_discussions_created_at')
+    this.schema.raw('DROP INDEX idx_discussions_created_at')
   }
 }
